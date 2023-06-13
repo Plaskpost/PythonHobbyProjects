e97af205822895687a80b13040df3d58b3325203
¨Plaskpost <aron.borjesson.lindgren@gmail.com>
2023-06-13 21:43:43 +0200

    Some 3D rendering up and running!!

--- HyperbolicMaze/Game.py
+++ HyperbolicMaze/Game.py
@@ -13,11 +13,11 @@ RIGHT = 1
 UP = 2
 LEFT = 3
 
-movement_speed = 0.4
-rotation_speed = 0.3  # Ok degrees
+movement_speed = 1.4
+rotation_speed = 1.3  # Ok degrees
 tile_size = 80
 player_radius = 10
-average_walls_per_tile = 2.8
+average_walls_per_tile = 2.0
 seed = 44
 fixed_seed = True
 
--- HyperbolicMaze/Rendering3D.py
+++ HyperbolicMaze/Rendering3D.py
@@ -11,8 +11,8 @@ class Rendering3D(Rendering):
 
     def __init__(self, dynamic_maze, explorer):
         super().__init__("First person view", dynamic_maze, explorer)
-        self.camera_span = (20, 60)  # Degrees
-        self.vertical_scale = 1000
+        self.camera_span = (90, 60)  # Degrees
+        self.vertical_scale = 50000
         self.camera_y_angle = 0
 
         self.wall_color = (255, 255, 255)
@@ -44,22 +44,21 @@ class Rendering3D(Rendering):
 
             self.add_wall_line(image_matrix, dist_to_wall[col], col, color)
 
-            pixel_array = pygame.surfarray.make_surface(image_matrix)
-            self.screen.blit(pixel_array, (0, 0))
-            pygame.display.flip()
+        pixel_array = pygame.surfarray.make_surface(image_matrix.swapaxes(0, 1))
+        self.screen.blit(pixel_array, (0, 0))
+        pygame.display.flip()
 
     def add_wall_line(self, matrix, distance, column, color):
         height = self.SCREEN_SIZE[1]
+        flipped_col_index = self.SCREEN_SIZE[0]-1 - column
         line_length = np.round(self.vertical_scale/distance).astype(int)
         # camera_shift = TODO: To be continued..
         start = max(0, (height - line_length) // 2)
         end = min(height-1, (height + line_length) // 2)
 
-        print(start, ", ", end)
-
-        matrix[start:end, column] = color  # TODO: height/width dimensioning is completely messed up
+        matrix[start:end, flipped_col_index] = color
         if end < height-1:
-            matrix[(end+1):, column] = self.floor_color
+            matrix[(end+1):, flipped_col_index] = self.floor_color
 
     def draw_overview_line(self, direction, distance, color):
         center = (self.SCREEN_SIZE[0] // 2, self.SCREEN_SIZE[1] // 2)
